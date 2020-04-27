ec342dd1843acb288a26bb6076e4688f986a087c
Joona Salven <joonaesa@metropolia.fi>
2020-04-27 20:17:00 +0300

    Added PWA to master

--- public/js/main.js
+++ public/js/main.js
@@ -1,63 +1,67 @@
-(async () => {
-    'use strict';
-
-    const apiURL = '/graphql';
-
-
-
+window.onload = async () => {
+  'use strict';
+  if ('serviceWorker' in navigator) {
+    try {
+      const worker = await navigator.serviceWorker.register('./sw.js');
+      console.log('Service Worker Registered');
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
 
 // general fetch from graphql API
-    const fetchGraphql = async (query) => {
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('token'),
-            },
-            body: JSON.stringify(query),
-        };
-        try {
-            const response = await fetch(apiURL, options);
-            const json = await response.json();
-            return json.data;
-        }
-        catch (e) {
-            console.log(e);
-            return false;
-        }
+  const apiURL = '/graphql';
+  const fetchGraphql = async (query) => {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token'),
+      },
+      body: JSON.stringify(query),
     };
+    try {
+      const response = await fetch(apiURL, options);
+      const json = await response.json();
+      return json.data;
+    }
+    catch (e) {
+      console.log(e);
+      return false;
+    }
+  };
 
 // indexedDB stuff
 //-------------------------------------------------------------
-const indexedDB = window.indexedDB || window.mozIndexedDB ||
-    window.webkitIndexedDB || window.msIndexedDB;
-const request = indexedDB.open('stationDB', 1);
-let db;
-request.onsuccess = (event) => {
+  const indexedDB = window.indexedDB || window.mozIndexedDB ||
+      window.webkitIndexedDB || window.msIndexedDB;
+  const request = indexedDB.open('stationDB', 1);
+  let db;
+  request.onsuccess = (event) => {
     db = request.result;
-};
+  };
 
-request.onupgradeneeded = () => {
+  request.onupgradeneeded = () => {
     let db = request.result;
     db.createObjectStore('stationList', {autoIncrement: true});
-};
-/*
-LOGIN
--------------------------------------------------------------
-*/
+  };
+  /*
+  LOGIN
+  -------------------------------------------------------------
+  */
 
-const loginForm = document.querySelector('.login-form');
+  const loginForm = document.querySelector('.login-form');
 
-const login = async (evt) => {
+  const login = async (evt) => {
     evt.preventDefault();
     let values = {};
     for (let i = 0; i < loginForm.elements.length; i++) {
-        if (loginForm.elements[i].tagName === 'INPUT')
-            values[loginForm.elements[i].name] = loginForm.elements[i].value;
+      if (loginForm.elements[i].tagName === 'INPUT')
+        values[loginForm.elements[i].name] = loginForm.elements[i].value;
     }
     const query = {
-        query: `{
+      query: `{
   login(username: "${values.username}", password: "${values.password}") {
     id
     username
@@ -67,35 +71,35 @@ const login = async (evt) => {
 `,
     };
     try {
-        const result = await fetchGraphql(query);
-        localStorage.setItem('token', result.login.token);
-        location.href = 'home.html';
+      const result = await fetchGraphql(query);
+      localStorage.setItem('token', result.login.token);
+      location.href = 'home.html';
     }
     catch (e) {
-        console.log('error', e.message);
+      console.log('error', e.message);
     }
 
-};
+  };
 
-loginForm.addEventListener('submit', login);
+  loginForm.addEventListener('submit', login);
 
-/*
-REGISTER
--------------------------------------------------------------
-*/
+  /*
+  REGISTER
+  -------------------------------------------------------------
+  */
 
-    const registerForm = document.querySelector('.register-form');
+  const registerForm = document.querySelector('.register-form');
 
-    registerForm.addEventListener('submit', async (evt) => {
-        evt.preventDefault();
-        let values = {};
-        for (let i = 0; i < registerForm.elements.length; i++) {
-            if (registerForm.elements[i].tagName === 'INPUT')
-                values[registerForm.elements[i].name] = registerForm.elements[i].value;
+  registerForm.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    let values = {};
+    for (let i = 0; i < registerForm.elements.length; i++) {
+      if (registerForm.elements[i].tagName === 'INPUT')
+        values[registerForm.elements[i].name] = registerForm.elements[i].value;
 
-        }
-        const mutation = {
-            query: `mutation {
+    }
+    const mutation = {
+      query: `mutation {
                  registerUser(username: "${values.username}", password: "${values.password}"){
                  id
                  username
@@ -103,26 +107,22 @@ REGISTER
                  }
                 }
             `,
-        };
+    };
 
-        try {
-            const result = await fetchGraphql(mutation);
+    try {
+      const result = await fetchGraphql(mutation);
 
-        }
-        catch (e) {
-            console.log('error', e.message);
-        }
+    }
+    catch (e) {
+      console.log('error', e.message);
+    }
 
-
-    });
-
-
-
+  });
 
 // check user token
-const checkUser = async () => {
+  const checkUser = async () => {
     const query = {
-        query: ` {
+      query: ` {
   user 
   {
     id
@@ -133,9 +133,9 @@ const checkUser = async () => {
 `,
     };
     const result = await fetchGraphql(query);
-     console.log(result);
+    console.log(result);
     if (result.user) {
     }
-};
+  };
 
-})();
+};
---
+++ public/sw.js
@@ -0,0 +1,33 @@
+'use strict';
+let cacheName = 'pwa';
+let filesToCache = [
+  './',
+  './css/main.css',
+  './index.html',
+  './js/main.js',
+  './favicon.ico',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil((async () => {
+    try {
+      const cache = await caches.open(cacheName);
+      return cache.addAll(filesToCache);
+    }
+    catch (e) {
+      console.log('after install', e.message);
+    }
+  })());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    try {
+      const response = await caches.match(e.request);
+      return response || fetch(e.request);
+    }
+    catch (e) {
+      console.log('load cache', e.message);
+    }
+  })());
+});
